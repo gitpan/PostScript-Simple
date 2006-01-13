8557b1871dc62c1a2c75753ddc893ff38423bf0d
Matthew C. Newton <perl03@newtoncomputing.co.uk>
2006-01-13 23:22:49 +0000

    Import of MCNEWTON/PostScript-Simple-0.07 from CPAN.
    gitpan-cpan-distribution: PostScript-Simple
    gitpan-cpan-version:      0.07
    gitpan-cpan-path:         MCNEWTON/PostScript-Simple-0.07.tar.gz
    gitpan-cpan-author:       MCNEWTON
    gitpan-cpan-maturity:     released

--- lib/PostScript/Simple/EPS.pm
+++ lib/PostScript/Simple/EPS.pm
@@ -169,7 +169,8 @@ sub _getfilebbox# {{{
   open EPS, "< $$self{file}" || croak "can't open eps file $$self{file}";
   SCAN: while (<EPS>)
   {
-    if (/^\%\%BoundingBox:\s+(-?\d+)\s+(-?\d+)\s+(-?\d+)\s+(-?\d+)$/)
+    s/[\r\n]*$//; #ultimate chomp
+    if (/^\%\%BoundingBox:\s+(-?\d+)\s+(-?\d+)\s+(-?\d+)\s+(-?\d+)\s*$/)
     {
       $$self{bbx1} = $1; 
       $$self{bby1} = $2; 
@@ -492,7 +493,7 @@ sub _get_include_data# {{{
   if ($$self{"clip"}) {
     $data .= "newpath $$self{bbx1} $$self{bby1} moveto
 $$self{bbx2} $$self{bby1} lineto $$self{bbx2} $$self{bby2} lineto
-$$self{bbx1} $$self{bby2} lineto closepath clip\n";
+$$self{bbx1} $$self{bby2} lineto closepath clip newpath\n";
   }
   if (defined $$self{"epsfile"}) {
     $data .= $$self{"epsfile"};
